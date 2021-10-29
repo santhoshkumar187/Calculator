b0b61836b552b547d0cced5fe53b1a0e980e0abb
siddarths1 <91017970+siddarths1@users.noreply.github.com>
2021-10-29 19:28:42 +0530

    reduced program

--- calculate.c
+++ calculate.c
@@ -2,14 +2,10 @@
 void main
 
 {
-	int n1;
-	int n2;
-	int sub;
-	printf("enter num 1:");
-	scanf("%d",&n1);
-	printf("enter num 2:");
-	scanf("%d",&n2);
-	sub  = n1-n2;
+	int n1,n2;
+	printf("enter num 1 and num 2:");
+	scanf("%d %d",&n1,&n2);
+	int sub  = n1-n2;
 	printf("subtraction is:",sub);
 }
 
