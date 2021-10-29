59a39a7475d7d29ba89b0a83297451d3f63c1fae
santhoshkumar187 <santhoshkumars.20cse@kongu.edu>
2021-10-29 19:10:11 +0530

    subtraction

---
+++ calculate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main
+
+{
+	int n1;
+	int n2;
+	int sub;
+	printf("enter num 1:");
+	scanf("%d",&n1);
+	printf("enter num 2:");
+	scanf("%d",&n2);
+	sub  = n1-n2;
+	printf("subtraction is:",sub);
+}
+
+
